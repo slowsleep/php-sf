b5da3bc4f10e01681b11cb9d0301e75a00677eeb
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-06-08 17:06:20 +0300

    Added all elems of calc

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,7 +3,6 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
-
 document.getElementById('btn_1').addEventListener('click', function () {
     inputWindow.value += '1';
 })
