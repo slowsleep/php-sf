767beb1bfdffec375b530f07502f9e131f86362e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-06-08 16:55:24 +0300

    Added calc, sum, def, and number 1.

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -4,6 +4,43 @@ let operation = null;
 const inputWindow = document.getElementById('inputWindow');
 
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    let result;
+    if (operation === 'sum') {
+        result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+     else if (operation === 'def') {
+        result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+
+})
+
+
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
