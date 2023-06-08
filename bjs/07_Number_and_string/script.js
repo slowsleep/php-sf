3317ecaf779795338ca0e68e27df535551e6173e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-06-08 17:53:23 +0300

    Added all calculator functions

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,41 +1,56 @@
+function sum(a, b) { return a + b;}
+
+function dif(a, b) { return a - b;}
+
+function mult(a, b) { return a * b;}
+
+function div(a, b) { return a / b;}
+
+function sqrt(a) { return Math.sqrt(a);}
+
+
 let lastOperand = 0;
 let operation = null;
-
 const inputWindow = document.getElementById('inputWindow');
+let nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-document.getElementById('btn_1').addEventListener('click', function () {
-    inputWindow.value += '1';
-})
+for (let num of nums) {
+    document.getElementById('btn_' + num).addEventListener('click', function () {
+        inputWindow.value += num;
+    })
+}
 
+let operands = ['sum', 'dif', 'mult', 'div', 'sqrt'];
 
-document.getElementById('btn_sum').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
-    operation = 'sum';
-    inputWindow.value = '';
+for (let operand of operands) {
+    document.getElementById('btn_' + operand).addEventListener('click', function () {
+        lastOperand = parseInt(inputWindow.value);
+        operation = operand;
+        inputWindow.value = '';
 
-})
-
-document.getElementById('btn_def').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
-    operation = 'def';
-    inputWindow.value = '';
-
-})
+    });
+}
 
 document.getElementById('btn_calc').addEventListener('click', function () {
     let result;
     if (operation === 'sum') {
-        result = lastOperand + parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+        result = sum(lastOperand, parseInt(inputWindow.value));
     }
-     else if (operation === 'def') {
-        result = lastOperand - parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
+    if (operation === 'dif') {
+        result = dif(lastOperand, parseInt(inputWindow.value));
     }
+    if (operation === 'mult') {
+        result = mult(lastOperand, parseInt(inputWindow.value));
+    }
+    if (operation === 'div') {
+        result = div(lastOperand, parseInt(inputWindow.value));
+    }
+    if (operation === 'sqrt') {
+        result = sqrt(lastOperand);
+    }
+    operation = null;
+    lastOperand = 0;
+    inputWindow.value = result;
 
 })
 
